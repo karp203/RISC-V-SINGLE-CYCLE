d804ce7d09ecfbca02b665514a71f078f033c420
karp203 <155094791+karp203@users.noreply.github.com>
2026-07-31 17:21:43 +0530

    Add files via upload

---
+++ RISC_V_SC/data_memory.sv
@@ -0,0 +1,48 @@
+// Data Memory - RTL (Refined, Parameterized)
+
+import risc_pkg::*;
+
+module data_memory #(
+  parameter ADDR_WIDTH = 8,  // 2^6 = 64KB (insted of 2^16)
+  parameter DATA_WIDTH = 8    // Byte-wide RAM
+)(
+  input logic 				  clk,
+  input  logic                dmem_req,                 
+  input  logic                dmem_wr_en,               
+  input mem_size_t		      dmem_data_size,           
+  input  logic [31:0]         dmem_addr,                
+  input  logic [31:0]         dmem_wr_data,             
+  input  logic                dmem_zero_extend,         
+
+  output logic [31:0]         dmem_rd_data              
+);
+
+    // RAM Declaration
+    logic [DATA_WIDTH-1:0] mem [0:(2**ADDR_WIDTH)-1];
+ 
+    always_ff @(posedge clk) begin
+      if (dmem_req && dmem_wr_en) begin
+        case (dmem_data_size)
+          MEM_BYTE: mem[dmem_addr] <= dmem_wr_data[7:0];
+          MEM_HALF: {mem[dmem_addr+1], mem[dmem_addr]} <= dmem_wr_data[15:0];
+          MEM_WORD: {mem[dmem_addr+3], mem[dmem_addr+1], mem[dmem_addr+1], mem[dmem_addr]} <= dmem_wr_data;
+        endcase
+      end
+    end
+
+    always_comb begin
+      if (dmem_req && !dmem_wr_en) begin
+        case (dmem_data_size)
+          MEM_BYTE: dmem_rd_data = dmem_zero_extend ?
+                          {{24{1'b0}}, mem[dmem_addr]} :
+                          {{24{mem[dmem_addr][7]}}, mem[dmem_addr]};
+          MEM_HALF: dmem_rd_data = dmem_zero_extend ?
+                          {{16{1'b0}}, mem[dmem_addr+1], mem[dmem_addr]} :
+                          {{16{mem[dmem_addr+1][7]}}, mem[dmem_addr+1], mem[dmem_addr]};
+          MEM_WORD: dmem_rd_data = {mem[dmem_addr+3], mem[dmem_addr+2], mem[dmem_addr+1], mem[dmem_addr]};
+        endcase
+      end else begin
+        dmem_rd_data = 32'h0;
+      end
+    end
+endmodule
---
+++ RISC_V_SC/register_file.sv
@@ -0,0 +1,37 @@
+module register_file (
+  input  logic         clk,
+  input  logic         reset_n,
+
+  // Read register addresses
+  input  logic [4:0]   rs1_addr,
+  input  logic [4:0]   rs2_addr,
+
+  // Write port
+  input  logic [4:0]   rd_addr,
+  input  logic         rf_wr_en,
+  input  logic [31:0]  wr_data,
+
+  // Read data outputs
+  output logic [31:0]  rs1_data,
+  output logic [31:0]  rs2_data
+);
+
+    // Register file: 32 x 32-bit registers
+    logic [31:0] regs [0:31];
+
+    always_ff @( posedge clk or negedge reset_n ) begin
+        if (!reset_n) begin
+            integer i;
+
+            for (i=0; i<32; i = i + 1)
+                regs[i] <= 32'h0;
+        end else if (rf_wr_en && (rd_addr != 5'h0)) begin
+            regs[rd_addr] <= wr_data;
+        end
+        
+    end
+
+    assign rs1_data = regs[rs1_addr];
+    assign rs2_data = regs[rs2_addr];
+endmodule
+
